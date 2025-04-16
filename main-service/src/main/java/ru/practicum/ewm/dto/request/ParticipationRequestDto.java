cb4178cf51e3e422775af1bbbe9d23dd5ced7a55
Ksenia Fedoseeva <merzlikina.ksu@gmail.com>
2025-04-17 00:53:14 +0300

    Реализован основной сервис в рамках проекта Explore With Me

--- main-service/src/main/java/ru/practicum/ewm/dto/request/ParticipationRequestDto.java
+++ main-service/src/main/java/ru/practicum/ewm/dto/request/ParticipationRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.ewm.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +21,6 @@ public class ParticipationRequestDto {
 
     private String status;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
 }
